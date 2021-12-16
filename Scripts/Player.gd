5291ca15d07ba7d3f57c22b5d0fc6150d190b334
Luiz Gustavo <lugu.almeida@gmail.com>
2021-12-16 00:34:02 -0300

    coinbox

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -144,3 +144,8 @@ func _on_hurtbox_body_entered(_body):
 
 func hit_checkpoint():
 	Global.checkpoint_pos = position
+
+
+func _on_Area2D_body_entered(body):
+	if body.has_method("destroy"):
+		body.destroy()
